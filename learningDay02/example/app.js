036179b6b9cf205983de3ab3891d0907b12b7c13
Tongyang Ni <ntyaiqingr@gmail.com>
2021-06-06 21:54:28 -0400

    event modifier in Vue

--- learningDay02/example/app.js
+++ learningDay02/example/app.js
@@ -2,8 +2,40 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      yourName: '',
+      confirmName: ''
     };
   },
+  methods: {
+    // vinilla javascript way when "v-on:submit"
+    /*     submitForm(event) {
+      event.preventDefault();
+      alert('Submitted');
+    }, */
+    // Vue way when "v-on:submit.prevent"
+    submitForm(){
+      alert('Submitted');
+    },
+    confirmInput() {
+      this.confirmName = this.yourName;
+    },
+    // function overloading is not supported by javascript
+    // add() {
+    //   this.counter++;
+    // },
+    // reduce() {
+    //   this.counter--;
+    // },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      this.yourName = event.target.value + ' ' + lastName;
+    }
+  }
 });
 
 app.mount('#events');
